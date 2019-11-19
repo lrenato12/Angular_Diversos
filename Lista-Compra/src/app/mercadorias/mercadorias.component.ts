45f2645e2ca0050975dd58032595d2ac15e1d00d
Luiz Renato <luiz.renato@consinco.com.br>
2019-11-19 09:43:18 -0300

    Aula 03

--- Lista-Compra/src/app/mercadorias/mercadorias.component.ts
+++ Lista-Compra/src/app/mercadorias/mercadorias.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mercadorias.component.css']
 })
 export class MercadoriasComponent implements OnInit {
+  title = "Lista de compras";
 
-  title = "Lista de Mercadorias";
+  mercadorias = [{
+    descricao: "Arroz",
+    quantidade: 1
+  },
+  {
+    descricao: "Feijão",
+    quantidade: 5
+  },
+  {
+    descricao: "Óleo",
+    quantidade: 4
+  }]
 
   constructor() { }
 
